a636ea63f51208505b253bccb54bf48b8e5d38fd
Miguel Munoz <memg92@gmail.com>
2020-10-09 18:23:36 -0300

    figured display of triangle

--- your-code/app.js
+++ your-code/app.js
@@ -4,21 +4,23 @@ window.addEventListener("DOMContentLoaded", (event) => {
   fetch("http://localhost:3001/api/path/")
     .then((res) => res.json())
     .then((res) => {
-      const rootNode = new DirectoryTreeNode("root", "directory", "2020-10-09" )
-      res.forEach(file => {
+      const rootNode = new DirectoryTreeNode("root", "directory", "2020-10-09");
+      res.forEach((file) => {
         let { name, type, lastModifiedTime } = file;
         rootNode.addChild(new DirectoryTreeNode(name, type, lastModifiedTime));
       });
       updateVisualTree(treeContainer, rootNode);
+      document.querySelector(".loading-overlay").style.display = "none";
       console.log(rootNode);
     });
+
+  document.querySelectorAll(".tree-entry__disclosure");
 });
 
 function updateVisualTree(element, directoryTreeNode) {
-
   // Create an unordered list to make a UI for the directoryTreeNode
-  const ul = document.createElement('ul');
-  ul.classList.add('tree');
+  const ul = document.createElement("ul");
+  ul.classList.add("tree");
 
   // Create a list element for every child of the directoryTreeNode
   for (let child of directoryTreeNode.children) {
@@ -30,30 +32,30 @@ function updateVisualTree(element, directoryTreeNode) {
 }
 
 function updateVisualTreeEntry(treeElement, child) {
-  const li = document.createElement('li');
-    li.classList.add('tree-entry');
+  const li = document.createElement("li");
+  li.classList.add("tree-entry");
 
-    // Create a list element with a file icon
-    if (child.type === 'file') {
-      li.innerHTML = `
-        <div class="tree-entry__disclosure tree-entry__disclosure--disabled></div>
-        <img class="tree-entry__icon" src="/icons/file_type_${child.getIconTypeName()}.svg">
+  // Create a list element with a file icon
+  if (child.type === "file") {
+    li.innerHTML =
+      '<div class="tree-entry__disclosure tree-entry__disclosure--disabled>\25B6</div>' +
+      `<img class="tree-entry__icon" src="/icons/file_type_${child.getIconTypeName()}.svg">
         <div class="tree-entry__name">${child.name}</div>
         <div class="tree-entry__time">${child.lastModifiedTime}</div>
       `;
 
     // Or create a list element with a folder icon
-    } else if (child.type === 'directory') {
-      li.innerHTML = `
+  } else if (child.type === "directory") {
+    li.innerHTML = `
         <div class="tree-entry__disclosure tree-entry__disclosure--closed"></div>
         <img class="tree-entry__icon" src="/icons/folder_type_${child.getIconTypeName()}.svg">
         <div class="tree-entry__name">${child.name}</div>
         <div class="tree-entry__time">${child.lastModifiedTime}</div>
       `;
-    }
+  }
 
-    // Add the newly created list element into the unordered list
-    treeElement.appendChild(li);
+  // Add the newly created list element into the unordered list
+  treeElement.appendChild(li);
 }
 
 class DirectoryTreeNode {
@@ -67,18 +69,18 @@ class DirectoryTreeNode {
     this.children.push(child);
   }
   getIconTypeName() {
-    if (this.type === 'directory') {
+    if (this.type === "directory") {
       return this.name;
     }
 
-    if (this.type === 'file') {
-      const dotIndex = this.name.lastIndexOf('.');
+    if (this.type === "file") {
+      const dotIndex = this.name.lastIndexOf(".");
       if (dotIndex >= 0) {
         return this.name.substring(dotIndex + 1).toLowerCase();
       }
       return this.name;
     }
 
-    return '';
+    return "";
   }
 }
